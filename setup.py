596e4145e03f28d35b140e0fe0df1c5523d6f1d7
highparkdev <joshclark@josh-clarks-mbp.swn.unco.edu>
2015-04-29 09:58:36 -0600

    commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'MesoPy',
+  packages = ['MesoPy'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'A pure python wrapper for the MesoWest API',
+  author = 'Josh Clark',
+  author_email = 'jclark754@gmail.com',
+  url = 'https://github.com/jclark754/MesoPy', # use the URL to the github repo
+  download_url = 'https://github.com/jclark754/MesoPy/tarball/1.0.0', 
+  keywords = ['weather', 'API', 'climate', 'MesoWest'], 
+  classifiers = [],
+)
