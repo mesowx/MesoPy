f59dd5035f9509b1153c85c43d2f71b991a7ad99
Joshua Clark <jclark754@gmail.com>
2015-05-27 21:12:47 -0600

    added all func tests

--- MesoPy/tests/test_main.py
+++ MesoPy/tests/test_main.py
@@ -1,29 +1,40 @@
 from MesoPy import MesoPy as meso
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 
-def testvarlist():
+
+# Basic Function Tests
+def testvarlistfunc():
     var_list = meso.variable_list()
-    print(var_list)
-    assert var_list
+    ok_(var_list)
 
-def testvar():
-    var_list = meso.variable_list()
-    assert 'relative_humidity' in var_list['VARIABLES']
 
-def teststations():
+def teststationsfunc():
     stations = meso.station_list(state='CO', county='Larimer')
-    print(stations)
-    assert 'KFNL' == stations['STATION'][1]['STID']
+    ok_('KFNL' == stations['STATION'][1]['STID'])
 
-def testlatest():
-    latest = meso.latest_obs(stid='kfnl,kden,kbou')
-    print(latest)
-    assert latest
+
+def testtimeseriesfunc():
+    timeseries = meso.timeseries_obs(stid='kfnl', start='201504261800', end='201504262300')
+    ok_(timeseries)
+
+
+def testclimatologyfunc():
+    climatology = meso.climatology_obs(stid='kden', startclim='04260000', endclim='04270000', units='precip|in')
+    ok_(climatology)
+
+
+def testprecipfunc():
+    precip = meso.precipitation_obs(stid='kfnl', start='201504261800', end='201504271200', units='precip|in')
+    ok_(precip)
+
+
+# Miscellaneous Tests
+def testvarexists():
+    var_list = meso.variable_list()
+    ok_('relative_humidity' in var_list['VARIABLES'])
+
 
 def testlateststrlist():
-    latest = meso.latest_obs(stid= ['kfnl', 'kden', 'ksdf'])
-    #assert len(latest['STATION'])==3
+    latest = meso.latest_obs(stid=['kfnl', 'kden', 'ksdf'])
+    # assert len(latest['STATION'])==3
     eq_(len(latest['STATION']), 3)
-
-
-
