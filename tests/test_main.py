1e1831347d75265bdae3f229d55559f8165565a4
Joshua Clark <jclark754@gmail.com>
2016-01-06 23:20:43 -0700

    more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -28,13 +28,35 @@ def testprecip():
     precip = m.precip(stid='kfnl', start='201504261800', end='201504271200', units='precip|in')
     ok_(precip)
 
+def testclimatestats():
+    climate_stats = m.climate_stats(stid='mtmet', startclim='03240000', endclim='03280000', type='all')
+    ok_(climate_stats)
+
+def testtimestats():
+    stats = m.time_stats(stid='mtmet', start='201403240000', end='201403280000', type='all')
+    ok_(stats)
+
+def testlatency():
+    latency = m.latency(stid='mtmet', start='201403240000', end='201403280000')
+    ok_(latency)
+
+def testnettypes():
+    nettypes = m.networktypes()
+    ok_(nettypes)
+
+def testnetworks():
+    networks = m.networks()
+    ok_(networks)
+
+def testattime():
+    attime = m.attime(stid='kfnl', attime='201504261800', within='30')
+    ok_(attime)
 
 # Miscellaneous Tests
 
 def testlateststrlist():
     latest = m.latest(stid="kfnl,kden,ksdf", within='90')
     eq_(len(latest['STATION']), 3)
-
 
 # Error Handling
 @raises(MesoPyError)
